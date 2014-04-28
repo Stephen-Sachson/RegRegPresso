8b3ea054069e159cd25c694075f58ddc42bdc564
Mafia <msx7000@gmail.com>
2014-04-28 13:25:25 +0800

    Adding regular expression function

--- RegPress/RegPress/ViewController.m
+++ RegPress/RegPress/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#define kTestText @"122haha34hahAa"
+#define kRegexPattern @"haha"
+
 @interface ViewController ()
 
 @end
@@ -18,6 +21,17 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSError *error;
+    
+    NSRegularExpression *regex=[[NSRegularExpression alloc] initWithPattern:kRegexPattern options:NSRegularExpressionCaseInsensitive error:&error];
+    if (!error) {
+        [regex enumerateMatchesInString:kTestText options:0 range:NSMakeRange(0, kTestText.length) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+            NSLog(@"{%lu, %lu} %@",(unsigned long)result.range.location,(unsigned long)result.range.length,[kTestText substringWithRange:result.range]);
+        }];
+    }
+    else {
+        NSLog(@"%@",error);
+    }
 }
 
 - (void)didReceiveMemoryWarning
